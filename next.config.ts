4d3c47591867b411c18ee149a7d1751b7b33f265
Bangladesh Associate of IT Solution <74583888+baitssoftware@users.noreply.github.com>
2025-03-10 10:13:17 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,7 @@ const nextConfig: NextConfig = {
       'shinystepsedu.com',
       'api.shinystepsedu.com',
       'test.bamsbd.com',
+      'dstqm.nedubd.com',
       'backend-beanibazar-ideal-madrasah.vercel.app',
     ],
   },
